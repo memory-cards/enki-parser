b092d966082ead25fb594cf2f0667fd5788a87aa
Vasiliy Vanchuk (@vvscode) <v.vanchuk@tut.by>
2019-03-04 11:25:28 +0300

    Extract questions information

--- index.js
+++ index.js
@@ -1,30 +1,62 @@
 const fsExtra = require('fs-extra');
 const remark = require('remark');
-const remarkFrontmatter = require('remark-frontmatter');
 const remarkHtml = require('remark-html');
-const util = require('util');
+const yaml = require('yaml');
+const PARTS_DELIMITER = '---';
 
 const md2Html = remark().use(remarkHtml);
-const md2object = remark().use(remarkFrontmatter, { preset: 'yaml' });
 
-md2Html.pro
+// I'm going to add __meta__ into question JSON
+// to put information about source / author
+const getMeta = str => {
+  let strParts = str.split(PARTS_DELIMITER);
+  const yamlContent = strParts[1].trim();
+  const yamlObj = yaml.parse(yamlContent);
+  return yamlObj;
+}
+
+const getQuestionData = str => {
+  const titleMatch = str.match(/## (.+)/);
+  const title = titleMatch ?  titleMatch[1] : 'item';
+  str = str.replace(titleMatch ? titleMatch[0] : '', '');
+
+  const answersMatches = (str.match(/\* (.+)/g) || []);
+  const answers = answersMatches.map(el => el.replace(/^\* /, ''));
+  answersMatches.forEach(el => str = str.replace(el, ''));
+  str = str.trim();
+
+  return {
+    answers,
+    html: md2Html.processSync(str),
+    numberOfCorrectAnswers: (str.match(/\?\?\?/g) || []).length,
+    title,
+  }
+}
+
+const getQuestions = str => {
+  const cardTitle = str.match(/# (.+)/)[1];
+
+  let strParts = str.split(PARTS_DELIMITER);
+  let questionParts = strParts.filter(el => el.includes('???'));
+  return questionParts.map(strPart => ({
+    cardTitle,
+    ...getQuestionData(strPart),
+  }));
+}
+
 
 const TEST_CARD_NAME = './input/curriculum/javascript/core/arrays/arrays.md';
 
 function processCardFile(fileName = TEST_CARD_NAME) {
   let outPutFileName = fileName.replace('input/', 'output/').replace('.md', '.html');
   let fileContent = fsExtra.readFileSync(fileName).toString();
-  let parsedContent = remark.parse(fileContent);
-
-  let html = md2Html.processSync(fileContent);
-  fsExtra.outputFileSync(outPutFileName, html);
-  fsExtra.outputFileSync(outPutFileName + '.json5', JSON.stringify(parsedContent, null, 2));
-  console.log('html', html);
-
+  let meta = getMeta(fileContent);
+  let questions = getQuestions(fileContent);
+  
+  console.log('meta', JSON.stringify(meta));
+  console.log('questions', questions);
 
   return 'result will be here';
 }
 
 processCardFile();
-
-
